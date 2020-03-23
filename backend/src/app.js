f5f2c62244baa6ce3a714835e81fb69b7cf75de6
Ivan Seibel <ivanseibel@gmail.com>
2020-03-23 15:42:03 -0300

    Implementing error tracking with sentry

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+import 'express-async-errors';
 
 import './database';
 import routes from './routes';
@@ -7,12 +11,15 @@ import routes from './routes';
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +29,15 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://b72fdb1d68ee489a992a080cc5295ed0@sentry.io/2902017',
+};
