2c63b8a78f10595bcdaab969de72b132feb5d1da
Ivan Seibel <ivanseibel@gmail.com>
2020-04-16 19:43:59 -0300

    Preparing login form to send data

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '../../assets/logo.png';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
-      <img src={logo} alt="GoBarber" />
+      <img src={logo} alt="FastFeet" />
 
-      <form>
-        <label htmlFor="email">YOUR EMAIL</label>
-        <input type="email" id="email" placeholder="example@email.com" />
-        <label htmlFor="email">YOUR PASSWORD</label>
-        <input type="password" id="password" placeholder="******" />
+      <Form onSubmit={handleSubmit}>
+        <strong>YOUR EMAIL</strong>
+        <Input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="example@email.com"
+        />
+        <strong>YOUR PASSWORD</strong>
+        <Input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="******"
+        />
 
         <button type="submit">Login</button>
-      </form>
+      </Form>
     </>
   );
 }
