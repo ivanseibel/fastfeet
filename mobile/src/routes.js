e91484cbe76509eb697c5e7e69f72120a91a7288
Ivan Seibel <ivanseibel@gmail.com>
2020-06-08 16:20:26 -0300

    Implemented tab navigation to Dashboard/Profile

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -2,17 +2,53 @@ import * as React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import SignIn from '~/pages/SignIn';
 import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabMain() {
+  return (
+    <Tab.Navigator
+      screenOptions={{ unmountOnBlur: true }}
+      tabBarOptions={{
+        activeTintColor: '#7D40E7',
+        labelStyle: { fontSize: 14 },
+      }}
+    >
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="list" color={color} size={20} />
+          ),
+        }}
+        name="Dashboard"
+        component={Dashboard}
+      />
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="account-circle" color={color} size={20} />
+          ),
+        }}
+        name="Profile"
+        component={Profile}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="Dashboard" component={TabMain} />
       </Stack.Navigator>
     </NavigationContainer>
   );
